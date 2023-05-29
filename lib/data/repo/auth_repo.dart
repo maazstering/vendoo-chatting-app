6dbba0d7bbbe97aee8dd03e20585985a3df3d774
Muhammad Maaz Siddiqui <maazsiddiqui.maaz2@gmail.com>
2023-05-29 11:22:09 +0500

    none

---
+++ lib/data/repo/auth_repo.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthRepo {
+  final _firebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void>signUp(String email, String password) async {
+    try {
+      FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: email, password: password);
+    }on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      }else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+      catch(e){
+        throw Exception(e.toString());
+      }
+
+  }
+}
