546dfb0c09ae08d80a07af14114acc303df585c4
Muhammad Maaz Siddiqui <maazsiddiqui.maaz2@gmail.com>
2023-05-29 15:52:14 +0500

    Nothing important

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:vendoo/signup.dart';
 import 'package:vendoo/chat.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'login.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
