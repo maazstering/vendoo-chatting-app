abb1c79f84ec6575429c357565f1961acaa15f49
Muhammad Maaz Siddiqui <maazsiddiqui.maaz2@gmail.com>
2023-05-29 11:34:15 +0500

    error

--- lib/data/repo/auth_repo.dart
+++ lib/data/repo/auth_repo.dart
@@ -1,21 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthRepo {
-  final _firebaseAuth _auth = FirebaseAuth.instance;
+  final _firebaseAuth = FirebaseAuth.instance;
 
-  Future<void>signUp(String email, String password) async {
+  Future<void> signUp(String email, String password) async {
     try {
-      FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: email, password: password);
-    }on FirebaseAuthException catch (e) {
+      FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
         print('The password provided is too weak.');
-      }else if (e.code == 'email-already-in-use') {
+      } else if (e.code == 'email-already-in-use') {
         print('The account already exists for that email.');
       }
-      catch(e){
-        throw Exception(e.toString());
-      }
-
+    } catch (e) {
+      throw Exception(e.toString());
+    }
   }
 }
