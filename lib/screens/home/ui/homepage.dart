d732c1788f54130d94faee4e7674e929c4a1bc45
Muhammad Maaz Siddiqui <maazsiddiqui.maaz2@gmail.com>
2023-06-01 22:37:17 +0500

    made the homepage choota

--- lib/screens/home/ui/homepage.dart
+++ lib/screens/home/ui/homepage.dart
@@ -5,6 +5,8 @@ import 'package:vendoo/screens/login/ui/login.dart';
 import 'package:vendoo/screens/signup/ui/signup.dart';
 import '../../../main.dart';
 import 'package:provider/provider.dart';
+import '../../../widgets/chatbutton.dart';
+import '../../../widgets/plainbutton.dart';
 import '../bloc/home_bloc.dart';
 
 class HomePage extends StatelessWidget {
@@ -95,109 +97,10 @@ class HomePage extends StatelessWidget {
                     ],
                   ),
                 ),
-                
               ],
             ),
           );
         },
-      ),
-    );
-  }
-}
-
-class LoginSignupButton extends StatelessWidget {
-  final String text;
-  final VoidCallback onPressed;
-
-  const LoginSignupButton({required this.text, required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 200,
-      height: 50,
-      child: ElevatedButton(
-        onPressed: onPressed,
-        style: ButtonStyle(
-          backgroundColor:
-              MaterialStateProperty.all<Color>(const Color(0xFFD9D9D9)),
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(25.0),
-              side: const BorderSide(color: Colors.grey), // Add border color
-            ),
-          ),
-          elevation: MaterialStateProperty.all<double>(8), // Adjust elevation
-        ),
-        child: Text(
-          text,
-          style: TextStyle(fontSize: 18, color: Colors.grey[800]),
-        ),
-      ),
-    );
-  }
-}
-
-class ChatButton extends StatelessWidget {
-  final VoidCallback onPressed;
-
-  const ChatButton({required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 200,
-      height: 50,
-      child: ElevatedButton(
-        onPressed: onPressed,
-        style: ButtonStyle(
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(28.0),
-            ),
-          ),
-          backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
-          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-          padding:
-              MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.zero),
-          overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
-          shadowColor: MaterialStateProperty.all<Color>(Colors.transparent),
-          minimumSize: MaterialStateProperty.all<Size>(const Size(0, 0)),
-          elevation: MaterialStateProperty.all<double>(0),
-          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-          textStyle: MaterialStateProperty.all<TextStyle>(
-            const TextStyle(
-              fontFamily: 'Inter',
-              fontWeight: FontWeight.bold,
-              fontSize: 17,
-              height: 1.24,
-            ),
-          ),
-        ),
-        child: Ink(
-          decoration: BoxDecoration(
-            gradient: const LinearGradient(
-              colors: [
-                Color(0xFF53EAEB),
-                Color(0xFF016BB4),
-                Color(0xFF2D3A93),
-                Color(0xFF8139B8),
-                Color(0xFF783BB3),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              stops: [0.0712, 0.285, 0.4562, 0.7251, 0.9251],
-            ),
-            borderRadius: BorderRadius.circular(28.0),
-          ),
-          child: Container(
-            alignment: Alignment.center,
-            child: const Text(
-              'Chat',
-              style: TextStyle(fontSize: 17),
-            ),
-          ),
-        ),
       ),
     );
   }
