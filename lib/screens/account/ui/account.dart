824937282eeca69b7a7e75e45a43061f4837e495
laiba-shah <121715553+laiba-shah@users.noreply.github.com>
2023-06-02 17:22:01 +0500

    update button added

--- lib/screens/account/ui/account.dart
+++ lib/screens/account/ui/account.dart
@@ -4,12 +4,64 @@ import '../../../api/apis.dart';
 import '../../../models/chat_user.dart';
 import 'package:vendoo/screens/home/ui/homepage.dart';
 
-class AccountPage extends StatelessWidget {
+class AccountPage extends StatefulWidget {
   late ChatUser user;
   //final FirebaseAuth auth = FirebaseAuth.instance;
   AccountPage({required this.user});
+
+  @override
+  _AccountPageState createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
+  bool isEditing = false;
+  bool showSaveButton = false;
+  late TextEditingController nameController;
+  late TextEditingController bioController;
+
+  @override
+  void initState() {
+    super.initState();
+    nameController = TextEditingController(text: widget.user.name);
+    bioController = TextEditingController(text: widget.user.about);
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    bioController.dispose();
+    super.dispose();
+  }
+
+  void toggleEditing() {
+    setState(() {
+      isEditing = !isEditing;
+      showSaveButton = !showSaveButton;
+    });
+  }
+
+  void saveName() {
+    // Save the edited name
+    String newName = nameController.text;
+    // Add your logic to save the name
+    // For example, you can call an API to update the user's name
+
+    toggleEditing(); // Exit edit mode after saving
+  }
+
+  void saveBio() {
+    // Save the edited bio
+    String newBio = bioController.text;
+    // Add your logic to save the bio
+    // For example, you can call an API to update the user's bio
+
+    toggleEditing(); // Exit edit mode after saving
+  }
+
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: true,
@@ -34,32 +86,62 @@ class AccountPage extends StatelessWidget {
             child: Icon(Icons.account_circle, size: 150),
           ),
           const SizedBox(height: 16.0),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text('${user.name}'),
-          ),
+          isEditing
+              ? TextFormField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    labelText: 'Name',
+                  ),
+                )
+              : ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('${widget.user.name}'),
+                ),
+          isEditing
+              ? TextFormField(
+                  controller: bioController,
+                  decoration: InputDecoration(
+                    labelText: 'Bio',
+                  ),
+                )
+              : ListTile(
+                  leading: Icon(Icons.info),
+                  title: Text('${widget.user.about}'),
+                  onTap: toggleEditing,
+                ),
           ListTile(
             leading: Icon(Icons.email),
-            title: Text('${user.email}'),
+            title: Text('${widget.user.email}'),
           ),
           ListTile(
             leading: Icon(Icons.edit),
             title: Text('Edit Profile'),
-            onTap: () {
-              // Handle edit profile action
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.logout),
-            title: Text('Sign Out'),
-            onTap: () {
-              signOut();
-              Navigator.push(context,
-                MaterialPageRoute(builder: (context) =>  HomePage(key: const ValueKey('myHomePage'))));
-            },
+            onTap: toggleEditing,
           ),
         ],
       ),
+      bottomNavigationBar: showSaveButton
+          ? ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: theme.primaryColor, // Use app's primary color as button background
+              ),
+              onPressed: () {
+                saveName();
+                saveBio();
+              },
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 12.0),
+                child: Text(
+                  'Save',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            )
+          : null,
     );
   }
 }
